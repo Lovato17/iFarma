ee2b4054b91428710488e81526e6bd29a06852a3
samucazikka007 <81575418+samucazikka007@users.noreply.github.com>
2021-04-26 17:30:56 -0300

    TELA DE PRODUTOS FINALIZADA
    a tela de produtos foi terminada agora só falta a tela de carrinho e arrumar o  layout das telas ;)

--- iFarma/app/src/main/java/com/example/ifarma/Carrinho.java
+++ iFarma/app/src/main/java/com/example/ifarma/Carrinho.java
@@ -2,15 +2,40 @@ package com.example.ifarma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Carrinho extends AppCompatActivity {
+
+
+    private Produtos classe = new Produtos();
+    private Double var=classe.total;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carrinho);
+
+
+
+        final EditText vl =(EditText)findViewById(R.id.edttotal);
+        vl.setEnabled(false);
+        vl.setText("R$ "+var.toString());
+
+
+
+
+
     }
+
+
+
 }
