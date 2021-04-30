ce6c7ba43dbe2738cb484a4d97c197e8c91d56ea
Renan F. Leite <68066944+RenanJoestar@users.noreply.github.com>
2021-04-30 00:13:38 -0300

    mudanças no design

--- iFarma/app/src/main/java/com/example/ifarma/Carrinho.java
+++ iFarma/app/src/main/java/com/example/ifarma/Carrinho.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,12 +20,21 @@ public class Carrinho extends AppCompatActivity {
     private String nome, resto;
     private double preco = 0, qtd = 0, precoTotal = 0;
     private int idTxt = 1, idBtn = 1;
+    private Button btn_voltar_produtos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carrinho);
 
+
+        btn_voltar_produtos=(Button)findViewById(R.id.btn_voltar_produtos);
+        btn_voltar_produtos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Carrinho.this, Produtos.class));
+            }
+        });
         final TextView ProdutoTXT = (TextView) findViewById(R.id.txtProdutos);
         ProdutoTXT.setText("");
 
